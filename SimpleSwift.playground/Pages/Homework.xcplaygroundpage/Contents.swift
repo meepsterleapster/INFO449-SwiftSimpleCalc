bacee90dad4ac8fef4fc3028ab63fe035420d3fd
George Lee <george10lee@gmail.com>
2025-04-05 23:55:23 -0700

    Complete Bonus Objectives

--- SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
+++ SimpleSwift.playground/Pages/Homework.xcplaygroundpage/Contents.swift
@@ -34,12 +34,20 @@ func calculate(_ arg: String) -> Int {
     var total = 0;
     var argLength = arg.count
     var curIndex = 0;
+    var negativeNum : Bool = false;
     for c in arg {
         curIndex+=1;
-        if (!c.isNumber && c != " ") {
+        if(c == "-"){
+            negativeNum = true;
+        }
+        else if (!c.isNumber && c != " ") {
             operation.append(c);
         }
         else if c.isNumber{
+            if(negativeNum == true){
+                tempNum.append("-")
+                negativeNum = false;
+            }
             tempNum.append(c);
             // If we reach end of the string,AND the end of the string is a number, make sure to append the number
             if(curIndex == argLength){
@@ -49,9 +57,18 @@ func calculate(_ arg: String) -> Int {
         }
         else if tempNum != ""{
             nums.append(Int(tempNum)!);
+            if(negativeNum == true){
+                negativeNum = false;
+                operation.append("-");
+            }
             tempNum = "";
         }
+        else if negativeNum == true{
+            negativeNum = false;
+            operation.append("-");
+        }
     }
+
     switch operation {
     case "+":
         total = nums[0] + nums[1];
@@ -80,7 +97,7 @@ func calculate(_ arg: String) -> Int {
             total =  recursiveFac(num: nums[0])
         }
     default:
-        total = -1;
+        total = -10;
     }
     return total;
 }
@@ -200,7 +217,7 @@ calculate("5 fact") == 120
 //: Implement `calculate([String])` and `calculate(String)` to handle negative numbers. You need only make the tests below pass. (You do not need to worry about "fact"/factorial with negative numbers, for example.)
 //:
 //: This is worth 1 pt
-/*
+
 calculate(["2", "+", "-2"]) == 0
 calculate(["2", "-", "-2"]) == 4
 calculate(["2", "*", "-2"]) == -4
@@ -215,7 +232,7 @@ calculate("2 - -2") == 4
 calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
-*/
+
  
 //: Implement `calculate([String])` and `calculate(String)` to use 
 //: and return floating-point values. You need only make the tests 
@@ -227,13 +244,115 @@ calculate("1 -2 3 -4 5 count") == 5
 //: Integer-based versions above.
 //: 
 //: This is worth 1 pt
-/*
+
 func calculate(_ args: [String]) -> Double {
-    return -1.0
-}
+    var nums: [Double] = [];
+    var total:Double = 0;
+    var  operation:String = "";
+    for string in args {
+        if let num = Double(string){
+            nums.append(num);
+        }
+        else {
+            operation = string;
+        }
+    }
+        switch operation {
+        case "+":
+            total = nums[0] + nums[1];
+        case "-":
+            total = nums[0] - nums[1];
+        case "/":
+            total = nums[0] / nums[1];
+        case "*":
+        total = nums[0] * nums[1];
+        case "%":
+            total = nums[0].truncatingRemainder(dividingBy: nums[1]) ;
+        case "count":
+            total = Double(nums.count);
+        case "avg":
+            total = nums.reduce(0, { x, y in
+                x + y
+            })
+            if(nums.count != 0){
+                total /= Double(nums.count)
+            }
+        default:
+            total = -1;
+        }
+    return total
+    }
+
 func calculate(_ arg: String) -> Double {
-    return -1.0
+
+    var operation: String = ""
+    var tempNum : String = ""
+    var nums:[Double] = [];
+    var total = 0.0;
+    var argLength = arg.count
+    var curIndex = 0;
+    var negativeNum : Bool = false;
+    for c in arg {
+        curIndex+=1;
+        if(c == "-"){
+            negativeNum = true;
+        }
+        else if c.isNumber || c == "."{
+            if(negativeNum == true){
+                tempNum.append("-")
+                negativeNum = false;
+            }
+            tempNum.append(c);
+            // If we reach end of the string,AND the end of the string is a number, make sure to append the number
+            if(curIndex == argLength){
+                nums.append(Double(tempNum)!);
+                tempNum = "";
+            }
+        }
+        else if (!c.isNumber && c != " ") {
+            operation.append(c);
+        }
+        else if tempNum != ""{
+            nums.append(Double(tempNum)!);
+            if(negativeNum == true){
+                negativeNum = false;
+                operation.append("-");
+            }
+            tempNum = "";
+        }
+        else if negativeNum == true{
+            negativeNum = false;
+            operation.append("-");
+        }
+    }
+    switch operation {
+    case "+":
+        total = nums[0] + nums[1];
+    case "-":
+        total = nums[0] - nums[1];
+    case "/":
+        total = nums[0] / nums[1];
+    case "*":
+    total = nums[0] * nums[1];
+    case "%":
+        total = nums[0].truncatingRemainder(dividingBy: nums[1]) ;
+    case "count":
+        total = Double(nums.count);
+    case "avg":
+        total = nums.reduce(0, { x, y in
+            x + y
+        })
+        if(nums.count != 0){
+            total /= Double(nums.count)
+        }
+    default:
+        total = -1;
+    }
+    return total;
 }
+
+
+
 
 calculate(["2.0", "+", "2.0"]) == 4.0
 calculate([".5", "+", "1.5"]) == 2.0
@@ -242,4 +361,4 @@ calculate(["2.5", "*", "2.5"]) == 6.25
 calculate(["2.0", "/", "2.0"]) == 1.0
 calculate(["2.0", "%", "2.0"]) == 0.0
 calculate("1.0 2.0 3.0 4.0 5.0 count") == 5.0
-*/
+
